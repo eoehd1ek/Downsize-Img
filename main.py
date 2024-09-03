77eb3ca1419ecdf4bba0424d743bd434e4f39a47
eoehd1ek <eoehd2ek@gmail.com>
2024-09-03 21:17:00 +0900

    Refacter: 핵심 로직 함수화

--- main.py
+++ main.py
@@ -13,6 +13,27 @@ HEIGHT_LIMIT = 1000
 #####################
 
 
+def get_result_image_size(x, y):
+    result_x = x
+    result_y = y
+    if (x == y):
+        # 1:1 비율일 때
+        result_x = min(x, HEIGHT_LIMIT)
+        result_y = min(y, HEIGHT_LIMIT)
+    else:
+        # 1:1이 아닐 땐
+        # 최대 ? x 1000 해상도로 맞추고 싶음.
+        if (y > HEIGHT_LIMIT):
+            result_x = int(x * HEIGHT_LIMIT / y)
+            result_y = int(y * HEIGHT_LIMIT / y)
+    return (result_x, result_y)
+
+
+def get_result_file_path(file_path):
+    result = f"{splitext(file_path)[0]}_pressed.jpg"
+    return result
+
+
 def main():
     if (len(sys.argv) <= 1):
         print("drag image files into this program. :)")
@@ -22,22 +43,9 @@ def main():
     for i in range(1, len(sys.argv)):
         try:
             image = Image.open(sys.argv[i]).convert("RGB")
-            [x, y] = image.size
-            result_x = x
-            result_y = y
-            if (x == y):
-                # 1:1 비율일 때
-                result_x = min(x, HEIGHT_LIMIT)
-                result_y = min(y, HEIGHT_LIMIT)
-            else:
-                # 1:1이 아닐 땐
-                # 최대 ? x 1000 해상도로 맞추고 싶음.
-                if (y > HEIGHT_LIMIT):
-                    result_x = int(x * HEIGHT_LIMIT / y)
-                    result_y = int(y * HEIGHT_LIMIT / y)
-            result_image = image.resize((result_x, result_y))
-            result_image.save(splitext(sys.argv[i])[
-                              0] + "_pressed.jpg", quality=IMAGE_QUALITY)
+            result_image = image.resize(get_result_image_size(*image.size))
+            result_image.save(get_result_file_path(
+                sys.argv[i]), quality=IMAGE_QUALITY)
             image.close()
             result_image.close()
         except:
